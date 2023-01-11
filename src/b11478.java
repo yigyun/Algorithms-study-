6711762e274c4b16b842e3e0d1899b1748d4c99e
igJung <akf151568@gmail.com>
2023-01-11 23:30:35 +0900

    b2

---
+++ src/b11478.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class b11478 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        HashSet<String> set = new HashSet<>();
+        String str = bf.readLine();
+        int c = 0;
+        int slength = str.length();
+        char[] art = new char[slength];
+        while(c < slength){
+            for(int i = 0; i < slength; i++) {
+                if(i+c >= slength)
+                    break;
+                set.add(str.substring(i,i+c+1));
+            }
+            c++;
+        }
+        System.out.println(set.size());
+    }
+}
--- src/b1254.java
+++ src/b1254.java
@@ -3,12 +3,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
+public class b1254 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
         //tringTokenizer st = new StringTokenizer(bf.readLine(), "", true);
-       // StringBuilder sb = new StringBuilder();
+        // StringBuilder sb = new StringBuilder();
         int length = str.length();
         for(int i = 0; i < length; i++)
         {
