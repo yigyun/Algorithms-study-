ed53c7c1c6b09759ef1379cd299e5eedaf55dbfc
igJung <akf151568@gmail.com>
2023-02-14 23:59:32 +0900

    2023-02-14

---
+++ src/b1013.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
+
+public class b1013 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringBuilder sb = new StringBuilder();
+        //StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        String pattern ="(100+1+|01)+";
+        for(int i = 0; i < N; i++){
+            boolean check = Pattern.matches(pattern, br.readLine());
+            if(check)   System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}
