a509476338d597808247b121aa3e28da208145b3
igJung <akf151568@gmail.com>
2023-02-15 16:41:25 +0900

    2023-02-15

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.regex.Pattern;
-
 public class Main {
     public static void main(String[] args) throws IOException {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            //StringBuilder sb = new StringBuilder();
-            //StringTokenizer st;
-            int N = Integer.parseInt(br.readLine());
-            String pattern ="(100+1+|01)+";
-            for(int i = 0; i < N; i++){
-                boolean check = Pattern.matches(pattern, br.readLine());
-                if(check)   System.out.println("YES");
-                else System.out.println("NO");
+            String str= br.readLine();
+            int[] num = new int[10];
+            int length = str.length();
+            int count = 0;
+
+            for(int i = 0; i < length; i++){
+                int temp = Integer.parseInt(str.substring(i, i+1));
+                num[temp] += 1;
+                count += temp;
+            }
+            if(count % 3 == 0 && str.contains("0")) {
+                for (int i = 9; i >= 0; i--) {
+                    while (num[i] > 0) {
+                        System.out.print(i);
+                        num[i]--;
+                    }
+                }
+            }
+            else{
+                System.out.println(-1);
             }
         }
     }
